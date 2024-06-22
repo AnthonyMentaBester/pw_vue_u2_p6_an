54e71b08fc4d94a9d64033eb2c33de862cb3a119
AnthonyMentaBester <ajnarvaezr@uce.edu.ec>
2024-06-21 19:47:37 -0500

    Taller22: ternimo del juego POKEMON API

--- src/components/PokemonOpciones.vue
+++ src/components/PokemonOpciones.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="opciones-container">
         <ul>
-            <li v-for="pokemon in pokemons" :key="pokemon.id">{{pokemon.name}}</li>
+            <li v-for="pokemon in pokemons" :key="pokemon.id" @click="$emit('seleccionPokemon', {ind:pokemon.id,nom:pokemon.nombre, test1:'esto es una prueba', test2:test3})">{{pokemon.nombre}}</li>
         </ul>
 
     </div>
@@ -9,6 +9,11 @@
 
 <script>
 export default {
+    data(){
+        return{
+            test3: 'prueba',
+        };
+    },
     props:{
         pokemons:{
             type: Array,
@@ -31,6 +36,7 @@ li{
     width: 250px;
     background-color: white;
     cursor: pointer;
+    padding: 20px;
 
 }
 
--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,7 +1,20 @@
 <template>
-<h1>Selecciona el Pokemon Correcto</h1>
-  <PokemonImagen :idPokemon="448" :mostrarPokemon="true"/>
-  <PokemonOpciones :pokemons="arreglo"/>
+  <h1 v-if="!pokemonCorrecto">Porfavor espere............</h1>
+  <div v-else>
+    <h1>Selecciona el Pokemon Correcto</h1>
+    <PokemonImagen :idPokemon="pokemonCorrecto.id" :mostrarPokemon="mostrar"/>
+    <PokemonOpciones v-show="!mensaje" :pokemons="arreglo" @seleccionPokemon="revisarRespuesta($event)"/>
+    <div v-if="inicio">
+      <p v-show="mensaje" > Haz seleccionado el pokemon correcto: {{nombre}}</p>
+    </div>
+
+    <div v-if="msjE">
+      <p v-show="mensaje" > Haz seleccionado el pokemon incorrecto: {{error}}</p>    
+    </div>
+    
+    
+
+  </div>
 </template>
 
 <script>
@@ -10,22 +23,49 @@ import PokemonOpciones from "../components/PokemonOpciones.vue";
 import obtenerPokemonsFachada from "../clientes/ClientePokemonAPI.js"
 
 export default {
+  data(){
+        return{
+            arreglo: [],
+            pokemonCorrecto: null,
+            mostrar : false,   
+            mensaje: false,
+            inicio: false,
+            msjE: false,
+            nombre: "",
+            error: "",
+        };
+    },
     components:{
         PokemonImagen,
         PokemonOpciones,
     },
     methods:{
       async cargaInicial(){
-        const vectorInicial= await obtenerPokemonsFachada(4);
+        const vectorInicial= await obtenerPokemonsFachada(7);
         this.arreglo = vectorInicial;
+        const indice = Math.floor(Math.random()*7);
+        this.pokemonCorrecto = this.arreglo[indice];
+      },
+      revisarRespuesta(dato){
+        console.log('se emitio un evento desde el hijo');
+        console.log(dato);
+        if (dato.ind === this.pokemonCorrecto.id) {
+          this.mostrar=true;
+          this.mensaje=true;
+          this.inicio= true;
+          this.nombre = dato.nom;
+        }else{
+          this.msjE = true;
+          this.mensaje=true;
+          this.error = "pokemon incorrecto"
+          console.log('ERROR')
+        }
+
       },
     },
-    data(){
-        return{
-            arreglo: [],
-        };
-    },
+
     mounted(){
+      console.log('Entro')
       this.cargaInicial();
 
     },
